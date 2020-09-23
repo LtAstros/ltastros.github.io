22b1d8c759a2639395247779ee5873bf0e5e570c
Costin Smilovici <costinsmilovici@Costin-MacBook-Air-128GB.local>
2020-09-23 15:23:14 -0700

    Updates

---
+++ precache-manifest.ddd602047f7ff0bb3e118ed8781f9b19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4b125fa55047502c44976d82f472ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d34344147731839025",
+    "url": "/static/css/main.c617c4a9.chunk.css"
+  },
+  {
+    "revision": "be9f63287a2bc2140899",
+    "url": "/static/js/2.9b712316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b712316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d34344147731839025",
+    "url": "/static/js/main.26d8e6bc.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "ff47a0cec0ca9d654d9b8c500bb58e94",
+    "url": "/static/media/Portrait.ff47a0ce.jpeg"
+  }
+]);
